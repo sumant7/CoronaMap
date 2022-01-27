1b7c5e4dc38330f72d3be07def6c411ba2e6e020
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-01-27 13:52:54 +0530

    map added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+//A Promise is a proxy for a value not necessarily known when the promise is created.
 function updateMap()
 {
-
-}
+    fetch("/data.json")    //simply gets data and returns a js promise
+    .then(response => response.json())
+    .then((rsp) =>{    //rsp is an object and rsp.data is an array
+        console.log(rsp.data)
+        rsp.data.forEach(element => {
+            lattitude= element.lattitude
+            longitude= element.longitude
+        });
+    })
+  }
 updateMap();
