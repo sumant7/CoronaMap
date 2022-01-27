725ab3836c6b3087a397896799c08ddb1cf19bb0
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-01-27 19:12:34 +0530

    non live completed

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 //A Promise is a proxy for a value not necessarily known when the promise is created.
-function updateMap()
-{
+function updateMap() {
+    console.log("Updating")
     fetch("/data.json")    //simply gets data and returns a js promise
-    .then(response => response.json())
-    .then((rsp) =>{    //rsp is an object and rsp.data is an array
-        console.log(rsp.data)
-        rsp.data.forEach(element => {
-            lattitude= element.lattitude
-            longitude= element.longitude
-        });
-    })
-  }
+        .then(response => response.json())
+        //rsp is an object and rsp.data is an array
+        .then((rsp) => {
+            //console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+                cases = element.infected;
+                x = ((cases) / 5 + 20) * 2;
+
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: `rgb(${x},0,0)`
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        })
+
+}
+
+interval = 2000;
+setInterval(updateMap, interval);
 updateMap();
